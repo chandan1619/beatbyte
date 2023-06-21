0da8b06d56b4dc014cc5c40b2bb72241dc8a89f4
chandan <chandancse.iitgn@gmail.com>
2023-06-21 17:41:41 +0530

    fixed the bug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 
-// const API_BASE_URL = "https://with-docker-api.onrender.com"; // Replace with your actual API URL
+const API_BASE_URL = "https://with-docker-api.onrender.com"; // Replace with your actual API URL
 
-const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = "http://localhost:8000";
 
 export default API_BASE_URL;
